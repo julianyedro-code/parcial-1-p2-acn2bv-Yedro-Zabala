97cf30fa69dbbf2f0504cfc0520f338e06dcdc05
julianyedro-code <julian.yedro@davinci.edu.ar>
2025-09-29 17:55:26 -0300

    Definicion de array asociativo con los 8 items

--- index.php
+++ index.php
@@ -1,6 +1,42 @@
 <?php 
 $pageTittle="Autos";
 
+$peugeot208=(object)[
+    "nombre"=>"peugeot 208",
+    "categoria" => "automovil",
+    "descripcion" =>"lorem impsum",
+    "imagen" => "https://www.media.stellantis.com/cache/a/d/d/4/5/add452e8f9f21b8c6903d8bbadc40f169a0bac6c.jpeg",
+    "precio" => "14,500,000",
+    "motor" => "1.6",
+    "potencia"=>"??",
+    "transmicion"=>"automatica"
+];
+$peugeot2008=(object)[
+    "nombre"=>"peugeot 208",
+    "categoria" => "automovil",
+    "descripcion" =>"lorem impsum",
+    "imagen" => "https://www.media.stellantis.com/cache/a/d/d/4/5/add452e8f9f21b8c6903d8bbadc40f169a0bac6c.jpeg",
+    "precio" => "14,500,000",
+    "motor" => "1.6",
+    "potencia"=>"??",
+    "transmicion"=>"automatica"
+
+];
+
+$peugeot308=(object)[
+    "nombre"=>"peugeot 208",
+    "categoria" => "automovil",
+    "descripcion" =>"lorem impsum",
+    "imagen" => "https://www.media.stellantis.com/cache/a/d/d/4/5/add452e8f9f21b8c6903d8bbadc40f169a0bac6c.jpeg",
+    "precio" => "14,500,000",
+    "motor" => "1.6",
+    "potencia"=>"??",
+    "transmicion"=>"automatica"
+];
+
+
+$listaPeugeot=[$peugeot208,$peugeot2008,$peugeot308];
+$cardsCount=count($listaPeugeot);
 
 ?>
 
@@ -19,21 +55,30 @@ $pageTittle="Autos";
 </head>
 <body>
     <header>
+        
+        <h1>Catálogo de Películas</h1>
 
     </header>  
     
     <main>
-        <div class="card" style="width: 18rem;">
+        <?php for($i=0 ; $i<$cardsCount ;$i++) { ?>
+           
+           <div class="card" style="width: 18rem;">
+                
+                <img src="<?=$listaPeugeot[$i]->imagen?>" class="card-img-top" alt="...">
             
-            <img src="..." class="card-img-top" alt="...">
-         
-               <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-        </div>
+                <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+
+        <?php } ?>
     </main>
+
+
+
     <footer></footer>  
 </body>
 </html>
